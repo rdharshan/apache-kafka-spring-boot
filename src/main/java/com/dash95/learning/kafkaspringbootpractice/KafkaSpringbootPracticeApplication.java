e639c101d0475c2185123ca2570be1853cf2d774
rdharshan <darshanramakrishna@gmail.com>
2021-05-08 23:15:05 +0530

    initial commit

---
+++ src/main/java/com/dash95/learning/kafkaspringbootpractice/KafkaSpringbootPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.dash95.learning.kafkaspringbootpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringbootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringbootPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dash95/learning/kafkaspringbootpractice/KafkaSpringbootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dash95.learning.kafkaspringbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringbootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
