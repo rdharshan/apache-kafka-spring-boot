22622c3e87cf2aa0dfbea47ac4c7589c834ec759
rdharshan <darshanramakrishna@gmail.com>
2021-05-09 16:24:10 +0530

    uncommented a code

--- src/main/java/com/dash95/learning/kafkaspringbootpractice/KafkaConsumerConfig.java
+++ src/main/java/com/dash95/learning/kafkaspringbootpractice/KafkaConsumerConfig.java
@@ -19,7 +19,7 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:2181");
-//        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props);
